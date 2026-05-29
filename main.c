ec41a029ed1af93219efd75f6634908196cf8fbf
rajsekharde <rajsekharde612@gmail.com>
2026-05-29 07:19:15 +0530

    Implement a queue

--- main.c
+++ main.c
@@ -3,16 +3,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void delayFunc() {
-    int t = 2;
-    printf("Sleeping for %d seconds\n", t);
-    sleep(t);
-}
-
 void* action(void *args) {
     int *a = (int*)args;
     printf("Printing from thread: %d\n", *a);
-    delayFunc();
+    sleep(2);
     free(a);
     pthread_exit(NULL);
 }
@@ -24,6 +18,15 @@ void singleThread() {
     pthread_create(&myThread, NULL, action, a);
     pthread_join(myThread, NULL);
     printf("Thread finished\n");
+}
+
+void sequentialExecution(int n)
+{
+    printf("Sequential Execution:\n");
+    for (int i = 0; i < n; i++)
+    {
+        sleep(2);
+    }
 }
 
 void multipleThreads(int n) {
@@ -41,16 +44,48 @@ void multipleThreads(int n) {
     free(threadArray);
 }
 
-void sequentialExecution(int n) {
-    printf("Sequential Execution:\n");
-    for(int i=0; i<n; i++) {
-        delayFunc();
+typedef struct Node {
+    int val;
+    struct Node* next;
+} Node;
+
+void push(Node** head, int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->val = value;
+    newNode->next = NULL;
+    if(!*head) {
+        *head = newNode;
+        return;
     }
+    Node* temp = *head;
+    while(temp->next != NULL) {temp = temp->next;}
+    temp->next = newNode;
+}
+
+void pop(Node** head) {
+    if(!*head) {return;}
+    Node *temp = *head;
+    *head = (*head)->next;
+    free(temp);
+}
+
+void displayQueue(Node* head) {
+    if(!head) {return;}
+    while(head) {
+        printf("%d ", head->val);
+        head = head->next;
+    }
+    printf("\n");
 }
 
 int main() {
-    singleThread();
-    sequentialExecution(10);
-    multipleThreads(10);
+    Node* head = NULL;
+    push(&head, 10);
+    push(&head, 20);
+    push(&head, 30);
+    displayQueue(head);
+    pop(&head);
+    printf("Front = %d\n", head->val);
+    displayQueue(head);
     return 0;
 }
