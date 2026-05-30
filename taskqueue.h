1b49880ac3731bdc303138e0c009ffd5a914a221
rajsekharde <rajsekharde612@gmail.com>
2026-05-30 09:59:24 +0530

    Define Task struct

--- taskqueue.h
+++ taskqueue.h
@@ -1,6 +1,11 @@
 #ifndef TASKQUEUE_H
 #define TASKQUEUE_H
 
-//
+// Task struct
+typedef struct Task {
+    void* (*function) (void*);
+    void* arg;
+    struct Task* next;
+} Task;
 
 #endif TASKQUEUE_H
